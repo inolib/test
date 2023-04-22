43d1aa4d4a0627c1c1aeb153476ffc3588a3fdf0
Matthieu MEIGNAN <matthieu.meignan@inolib.com>
2023-04-22 05:00:48 +0200

    refactor: app

--- src/index.ts
+++ src/index.ts
@@ -8,41 +8,33 @@ import { createYoga } from "graphql-yoga";
 
 import { contactModule, documentsModule, scalarModule, userModule } from "./modules";
 
-const makeApp = async () => {
-  const yoga = createYoga({
-    plugins: [
-      useGraphQLModules(
-        createApplication({
-          modules: [contactModule, documentsModule, scalarModule, userModule],
-        })
-      ),
-    ],
-    context: {
-      prisma: new PrismaClient(),
-    },
-    cors: {
-      allowedHeaders: ["Content-Type"],
-      methods: ["POST"],
-      origin: process.env.CORS_ORIGIN ?? "*",
-    },
-    graphiql: false,
-    graphqlEndpoint: "/api",
-    landingPage: false,
-  });
+const yoga = createYoga({
+  plugins: [
+    useGraphQLModules(
+      createApplication({
+        modules: [contactModule, documentsModule, scalarModule, userModule],
+      })
+    ),
+  ],
+  context: {
+    prisma: new PrismaClient(),
+  },
+  cors: {
+    allowedHeaders: ["Content-Type"],
+    methods: ["POST"],
+    origin: process.env.CORS_ORIGIN ?? "*",
+  },
+  graphiql: false,
+  graphqlEndpoint: "/api",
+  landingPage: false,
+});
 
-  const app = express();
+const app = express();
 
-  app.use(yoga);
+app.use(yoga);
 
-  return Promise.resolve(app);
-};
-
-const appPromise = makeApp();
-
-export const viteNodeApp = appPromise;
+export const viteNodeApp = app;
 
 export default async (request: Request, response: Response) => {
-  const app = await appPromise;
-
-  app(request, response);
+  await app(request, response);
 };
